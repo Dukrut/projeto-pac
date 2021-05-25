153c53578054bbfd2656fdf207bbbf8f10fafd2e
Eduardo <eduardo-krutzsch@hotmail.com>
2021-05-24 21:49:51 -0300

    Alterado o framework Vuetify para Bootstrap-vue e feito ajustes na tela de login

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
 import toast from './plugins/toast'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/js/bootstrap.js";
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
   toast,
   render: h => h(App)
 }).$mount('#app')
