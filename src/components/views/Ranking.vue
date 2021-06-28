0c34ca7485eb4732b951c5a5e8b1d15745802428
Braiancz <54813335+Braiancz@users.noreply.github.com>
2021-06-28 20:22:10 -0300

    Update Ranking.vue

--- src/components/views/Ranking.vue
+++ src/components/views/Ranking.vue
@@ -2,150 +2,134 @@
   <div class="body p-4">
     <h3><b-icon icon="people"></b-icon> Ranking</h3>
     <hr>
-    <table class="w-100 border">
-      <thead class="text-center border header-table">
-        <th class="p-2" v-for="field, index in fields" :key="index">{{field.name}}</th>
-      </thead>      
-      <tbody class="border">
-        <tr class="border" :class="{'item-table-striped':index%2 == 0, 'item-table': index%2 != 0}" v-for="item,index in items" :key="index">
-          <td class="p-2 border text-justify"> {{item.position}} </td>
-          <td class="p-2 border text-justify">{{item.name}}</td>
-          <td class="p-2 border text-justify"> {{item.description}} </td>
-        </tr>
-      </tbody>
-    </table>
+
+    <div class="container">
+        <div id="podium">
+            <div class="podiumPosition" id="secondPlace"><b>{{podium[1].id}}</b></div>
+            <div class="podiumPosition" id="firstPlace"><b>{{podium[0].id}}</b></div>
+            <div class="podiumPosition" id="thirdPlace"><b>{{podium[2].id}}</b></div>
+        </div>
+        <div id="labels">
+            <div class="podiumName" id="secondName">{{podium[1].name}}</div>
+            <div class="podiumName" id="firsName">{{podium[0].name}}</div>
+            <div class="podiumName" id="thrirdName">{{podium[2].name}}</div>
+        </div>
+    </div>
+    <div class="container" style="margin-top: -50px"> <!-- Tabela vai aqui -->
+        <table class="w-100 border">
+          <thead class="text-center border header-table">
+            <th class="p-2" v-for="field, index in fields" :key="index">{{field.name}}</th>
+          </thead>      
+          <tbody class="border">
+            <tr class="border" :class="{'item-table-striped':index%2 == 0, 'item-table': index%2 != 0}" v-for="item, index in items" :key="index">
+              <td class="p-2 border text-justify"> {{item.position}} </td>
+              <td class="p-2 border text-justify">{{item.name}}</td>
+              <td class="p-2 border text-justify"> {{item.description}} </td>
+            </tr>
+          </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
-<script>
-
-export default {
-  components:{
-  },
-  data: () => ({
-    fields: [
-      {name: "Posição"},
-      {name: "Nome" },
-      {name: "Pontuação"}
-    ],
-    items: [
-      {
-        position: "1º Lugar",
-        name: 'Braian Costa Zapelini',
-        description: "100",
-        id: 1
-      },
-      {
-        position: "2º Lugar",
-        name: 'Gusthawo Junkes',
-        description: "90",
-        id: 2
-      },
-      {
-        position: "3º Lugar",
-        name: 'Sílvio Baião Amarante',
-        description: "80",
-        id: 3
-      },
-      {
-        position: "4º Lugar",
-        name: 'Sasha Quintal da Silva',
-        description: "70",
-        id: 4
-      },
-      {
-        position: "5º Lugar",
-        name: 'Brenda Pereira',
-        description: "60",
-        id: 5
-      },
-      {
-        position: "6º Lugar",
-        name: 'Eduardo Krutsch',
-        description: "50",
-        id: 6
-      },
-      {
-        position: "7º Lugar",
-        name: 'Márcia Belmira',
-        description: "40",
-        id: 7
-      },
-      {
-        position: "8º Lugar",
-        name: 'Ana Julia',
-        description: "30",
-        id: 8
-      }
-    ],
-    remove_group:{
-      name: null,
-      id: null
-    },
-    edit_group:{
-      name: null,
-      id: null,
-      description: null,
-      permissions: {
-        modules:{
-          challenges: false,
-          ranking: false,
-          reports: false,
-          configuration: false
-        },
-        actions:{
-          conf_chanllenges:false,
-          view_challenges: false,
-          play_challegens: false,
-          conf_users: false,
-          view_users: false,
-          conf_groups: false,
-          view_groups: false
-        }
-      }
-    },
-    new_group:{
-      name: null,
-      description: null,
-      permissions: {
-        modules:{
-          challenges: true,
-          ranking: true,
-          reports: false,
-          configuration: false
-        },
-        actions:{
-          conf_chanllenges:false,
-          view_challenges: true,
-          play_challegens: true,
-          conf_users: false,
-          view_users: false,
-          conf_groups: false,
-          view_groups: false
-        }
-      }
-    },
-    error_new_group:{
-      name: null,
-      description: null,
-    },
-    error_edit_group:{
-      name: null,
-      description: null
-    }
-  }),
-  methods: {
-
-    regexTypes: function() {
-      return [null, undefined, ""]
-    },
-
-  }
-}
-
-</script>
-
 <style>
+
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .container {
+    width: 70%;
+    height: 40%;
+    margin: auto;
+  }
+  #podium, #labels {
+    width: 49%;
+    height: 40%;
+    margin: auto;
+  }
+  #podium {
+    margin-top: 70px;
+    margin-left: 25%;
+    position: relative;
+  }
+  .podiumPosition, .podiumName {
+    width: 190px;
+    float: left;
+    margin: 0 10px 0 10px;
+    border-radius: 15px;
+    text-align: center;
+  }
+  .podiumPosition { 
+    color: white; 
+    font-size: 25px;
+  }
+  .podiumName {
+    width: 190px;
+    margin: 0 7.7px 0 7.7px;
+    animation: podiumName 2.1s linear;
+    font-size: 20px;
+  }
+  #firstPlace {
+    background: #FFD700;
+    height: 120px;
+    line-height: 120px;
+    animation: firstPlace .7s linear;
+  }
+  #secondPlace {
+    background: #C0C0C0;
+    margin-top: 40px;
+    height: 80px;
+    line-height: 80px;
+    animation: secondPlace 1.4s linear;
+  }
+  #thirdPlace {
+    background: #CD7F32;
+    margin-top: 60px;
+    height: 60px;
+    line-height: 60px;
+    animation: thirdPlace 2.1s linear;
+  }
+  @keyframes firstPlace {
+    from {
+        opacity: 0;
+        margin-top: 115px;
+        height: 5px;
+    }
+    to {
+        opacity: 1;
+        height: 120px;
+    }
+  }
+  @keyframes secondPlace {
+    from {
+        opacity: 0;
+        margin-top: 115px;
+        height: 5px;
+    }
+    to {
+        opacity: 1;
+        margin-top: 40px;
+        height: 80px;
+    }
+  }
+  @keyframes thirdPlace {
+    from {
+        opacity: 0;
+        margin-top: 115px;
+        height: 5px;
+    }
+    to {
+        opacity: 1;
+        margin-top: 60px;
+        height: 60px;
+    }
+  }
+  @keyframes podiumName {
+    from { opacity: 0;}
+    to { opacity: 1;}
+  }
+
+  /*-----------------------------------*/
 
   .nav-buttons{
     background-color: #D5EAFF;
@@ -189,3 +173,78 @@ export default {
   }
 
 </style>
+
+<script>
+
+export default {
+  components:{
+  },
+  data: () => ({
+    fields: [
+      {name: "Posição"},
+      {name: "Nome" },
+      {name: "Pontuação"}
+    ],
+    podium: [
+      {
+        position: "1º Lugar",
+        name: 'Braian Costa Zapelini',
+        description: "100",
+        id: 1
+      },
+      {
+        position: "2º Lugar",
+        name: 'Gabriel da Costa',
+        description: "90",
+        id: 2
+      },
+      {
+        position: "3º Lugar",
+        name: 'Leonardo Cech',
+        description: "80",
+        id: 3
+      }
+    ],
+    items: [
+      {
+        position: "4º Lugar",
+        name: 'Siomar Komcê',
+        description: "70",
+        id: 4
+      },
+      {
+        position: "5º Lugar",
+        name: 'Brenda Pereira',
+        description: "60",
+        id: 5
+      },
+      {
+        position: "6º Lugar",
+        name: 'Tadeu Ximitz',
+        description: "50",
+        id: 6
+      },
+      {
+        position: "7º Lugar",
+        name: 'Márcia Belmira',
+        description: "40",
+        id: 7
+      },
+      {
+        position: "8º Lugar",
+        name: 'Ana Julia',
+        description: "30",
+        id: 8
+      }
+    ],
+  }),
+  methods: {
+
+    regexTypes: function() {
+      return [null, undefined, ""]
+    }
+
+  }
+}
+
+</script>
