e121efdab60a7e099a1b02f205432bdaa5cf8d38
Eduardo <eduardo-krutzsch@hotmail.com>
2021-07-06 20:32:58 -0300

    Refatorado o Challenges para ter melhor usabilidade.

--- src/components/views/Challenges.vue
+++ src/components/views/Challenges.vue
@@ -5,54 +5,88 @@
     <hr>
 
     <div v-if="!started">
-      <h5>Olá <b>{{user.name}}</b>, você já completou 30 de 100 desafios.</h5>
-      <div class="col-12 d-flex justify-content-center align-middle h-25">
-        <img src="../../../public/images/questions.svg" class="rounded-3" style="height:62.4vh" alt="">
+      <h5>Olá <b>{{user.name}}</b>! É muito bom te ver aqui!</h5>
+      <div class="w-100 text-center">
+        <h3 for="">Selecione nível de dificuldade para iniciar os desafios.</h3>
+        
+        <b-form-group>
+          <b-form-radio-group
+            id="radio-group-1"
+            v-model="level"
+            :options="options"
+            name="radio-options"
+          ></b-form-radio-group>
+        </b-form-group>
+
+        <div class="col-12 d-flex justify-content-center align-middle h-25">
+          <img src="../../../public/images/questions.svg" class="rounded-3" style="height:62.4vh" alt="">
+        </div>
+        
+        <b-button class="buttons w-25 mt-4" @click="startChallenges()">Iniciar Desafios</b-button>
       </div>
+    </div>
 
-      <b-form-group label="Selecione o nível de dificuldade" v-model="level">
-        <b-form-radio value="1" v-model="level">&nbsp;Fácil</b-form-radio>
-        <b-form-radio value="2" v-model="level">&nbsp;Médio</b-form-radio>
-        <b-form-radio value="3" v-model="level">&nbsp;Difícil</b-form-radio>
-     </b-form-group>
-
-     <div class="w-100 text-center">
-       <b-button class="buttons w-25 mt-4" @click="startChallenges()">Iniciar Desafios</b-button>
-     </div>
-  </div>
-
-  <div v-else>
-    <h5 v-if="key == 0">Boa sorte <b>{{user.name}}</b>! Preste muita atenção nas perguntas para responde-las corretamente.</h5>
-    <div class="d-flex flex-column bd-highlight mb-3 overflow-auto">
-      <div class="p-1 bd-highlight">
-        <span>{{ key + 1 }}. <strong>Descrição</strong>: {{ challenges[key].description }}</span>
+    <div v-else class="w-100">
+      <div class="row">
+        <div class="col-8">
+          <div class="p-1">
+            <span>{{ labelAlternative(key) }}. <strong>Descrição da questão</strong></span><br>
+            <p class="ml-4"> {{ challenges[key].description }} </p>
+          </div>
+          
+          <div class="p-1">
+            <span><strong>Selecione a alternativa correta</strong>:</span>
+            <b-form-radio-group
+                  v-model="answer[key]"
+                  :options="challenges[key].options"
+                  :name="answer[key]"
+            ></b-form-radio-group>
+          </div>
+          <hr>  
+        <div class="d-flex justify-content-center align-middle h-50">
+          <b-button class="buttons mt-4 btn" @click="backQuestion()" style="height:38px" :disabled="key == 0"><b-icon icon="arrow-left-square"></b-icon> Voltar pergunta</b-button>
+          <b-button v-if="key != 4" class="buttons btn mx-2 mt-4" style="height:38px" @click="nextQuestion()" :disabled="regexTypes().includes(answer[key])">Próxima pergunta <b-icon icon="arrow-right-square"></b-icon></b-button>
+          <b-button v-else class="buttons mx-2 mt-4 btn" style="height:38px" @click="nextQuestion()">Enviar respostas <b-icon icon="check-circle"></b-icon></b-button>
+        </div>
+        </div>
+        <div class="col-4 border-left div-left p-2">
+          <div v-if="regexTypes().includes(answer[4])" class="d-flex justify-content-center align-middle w-100 timer">
+            <h1><b-icon icon="stopwatch"></b-icon> Tempo restante:<span class="ml-4" :style="'color:'+color">{{ time }}</span></h1>
+          </div>
+          <hr>
+          <div class="ml-3 p-2">
+            <h3 class="text-center mt-3">Perguntas respondidas</h3>
+            <div v-for="ans,index in answer"  :key="index" class="row mt-3">
+              <strong>{{ index+1 }}</strong>.
+              <div class="ml-3 p-2 rounded border" :style="'background-color:' + (regexTypes().includes(answer[index]) ? 'orange' : 'green') + '; width:25px'"/>
+              <div class="ml-3">
+                <span v-if="regexTypes().includes(answer[index])">
+                  Essa questão ainda não foi respondida.
+                </span>
+                <span v-else>
+                  Essa questão foi respondida.
+                </span>
+                </div>
+            </div>
+          </div>
+        <hr>
+        <div class="mt-3 p-2">
+          <h3 class="text-center mt-3">Gabarito</h3>
+          <div class="w-100">
+            <span class="text-center" v-if="regexTypes().includes(answer[4])"> <i class="text-center">É necessário responder os 5 desafios antes de ver o gabarito.</i></span>
+            <div>
+              <p v-for="ans,index in answer"  :key="index" class="mt-3 ml-2"> <b>{{labelAlternative(index)}}</b>. {{answer[index]}}</p>
+              <div class="d-flex justify-content-center align-middle h-50">
+                <b-button class="buttons btn" @click="submitReply()">Enviar respostas</b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        </div>
       </div>
-      <div class="p-1 bd-highlight">
-        <span><strong>Selecione a alternativa correta</strong>:</span>
-        <b-form-radio-group
-                v-model="answer[key]"
-                :options="challenges[key].options"
-                :name="answer[key]"
-                inline
-        ></b-form-radio-group>
-      </div>
-      <hr>
+      
     </div>
-    <div class="d-flex justify-content-center align-middle h-50">
-      <b-button class="buttons mt-4" @click="backQuestion()"><b-icon icon="arrow-left-square"></b-icon> Voltar pergunta</b-button>
-      <b-button v-if="key != 4" class="buttons mx-2 mt-4" @click="nextQuestion()">Próxima pergunta <b-icon icon="arrow-right-square"></b-icon></b-button>
-      <b-button v-else class="buttons mx-2 mt-4" @click="nextQuestion()">Enviar respostas <b-icon icon="check-circle"></b-icon></b-button>
-    </div>
-    <div v-if="regexTypes().includes(answer[4])" class="d-flex justify-content-center align-middle w-100 timer">
-      <h1><b-icon icon="stopwatch"></b-icon> Tempo restante:<span class="ml-4" :style="'color:'+color">{{ time }}</span></h1>
-    </div>
-    <div v-if="key == 4 && !regexTypes().includes(answer[4])" class="align-middle">
-      <h2>GABARITO</h2>
-      <p v-for="ans,index in answer"  :key="index"><strong>{{ index+1 }}</strong>. {{ answer[index]}}</p>
-      <h3><b-icon icon="stopwatch"></b-icon> Tempo total:<span class="ml-4" :style="'color:'+color">{{ time }}</span></h3>
-    </div>
-  </div>
-
   </div>
 </template>
 
@@ -70,7 +104,12 @@ export default {
     answer: [null, null, null, null, null],
     time: "",
     color: "green",
-    key: 0
+    key: 0,
+    options: [
+      {text: 'Fácil',    value: '1'},
+      {text: 'Moderado', value: '2'},
+      {text: 'Difícil',  value: '3'}
+    ]
   }),
 
   mounted() {
@@ -91,6 +130,7 @@ export default {
         let questions = response.data 
         if (!questions.length) {
           service._toast("Não há questões desse nível cadastradas no banco de questões, acione o seu professor!", "error")  
+          return false
         }
         for (let index in questions) {
           let question = questions[index]
@@ -115,6 +155,19 @@ export default {
       })
     },
 
+    labelAlternative: function(key){
+      if (key == 0)
+        return "A"
+      else if (key == 1)
+        return "B"
+      else if (key == 2)
+        return "C"
+      else if (key == 3)
+        return "D"
+      else if (key == 4)
+        return "E"
+    },
+
     _toast: function(_message, _type){
       this.$toast.open({
         message: _message,
@@ -133,25 +186,28 @@ export default {
     },
 
     nextQuestion: function(){
-      if(this.key == 4)
-        return false
+      setTimeout(() => {  
+        if(this.key == 4)
+          return false
+        if (this.regexTypes().includes(this.answer[this.key])) {
+          this._toast("Selecione uma resposta antes de ir para a próxima pergunta", "error")
+          return false;
+        }
 
-      if (this.regexTypes().includes(this.answer[this.key])) {
-        this._toast("Selecione uma resposta antes de ir para a próxima pergunta", "error")
-        return false;
-      }
-
-      this.key++
+        this.key++
+      }, 150);
     },
 
     backQuestion: function(){
-      if(this.key == 0)
-        return false
-      this.key--
+      setTimeout(() => {
+        if(this.key == 0)
+          return false
+        this.key--
+      }, 150);
     },
 
-    timeChallenge(){
-      if(!this.started || this.key == 4)
+    timeChallenge: function(){
+      if(!this.started || (this.key == 4 && !this.regexTypes().includes(this.answer[this.key])))
         return false
 
       var aux = this.time.split(":")
@@ -180,6 +236,10 @@ export default {
         seconds = "0" + String(seconds)
 
       this.time = minute + ":" + seconds;
+    },
+
+    submitReply: function(){
+
     }
 
   }
@@ -194,8 +254,13 @@ export default {
   .buttons{
     background-color: #1A526B !important;
   }
+  
+  .div-left{
+    height: 83vh;
+  }
 
   .timer{
     margin-top: 10%;
   }
+
 </style>
